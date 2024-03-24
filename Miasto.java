89543c3b3a064030b6176fe56b88d1e5f181b606
MikolajKa <miki@radzymin.com.pl>
2024-03-25 00:05:16 +0100

    dodawanie

---
+++ Miasto.java
@@ -0,0 +1,18 @@
+public class Miasto {
+    private String nazwa;
+
+    public Miasto(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    @Override
+    public String toString() {
+        return "Miasto{" +
+                "nazwa='" + nazwa + '\'' +
+                '}';
+    }
+}
