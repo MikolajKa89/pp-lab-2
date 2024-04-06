410cd974752ff08d2fc7008adca600ec5dd22d9c
MikolajKa89 <164363596+MikolajKa89@users.noreply.github.com>
2024-04-06 12:12:30 +0200

    Update zad1.java

--- Miasto.java
+++ Miasto.java
@@ -1,18 +1,28 @@
-public class Miasto {
-    private String nazwa;
+import java.util.Scanner;
 
-    public Miasto(String nazwa) {
-        this.nazwa = nazwa;
-    }
+public class ŚredniaOcen {
 
-    public String getNazwa() {
-        return nazwa;
-    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    @Override
-    public String toString() {
-        return "Miasto{" +
-                "nazwa='" + nazwa + '\'' +
-                '}';
+        System.out.println("Podaj liczbę przedmiotów: ");
+        int liczbaPrzedmiotów = scanner.nextInt();
+
+        double[] oceny = new double[liczbaPrzedmiotów];
+
+        for (int i = 0; i < liczbaPrzedmiotów; i++) {
+            System.out.println("Podaj ocenę z przedmiotu " + (i + 1) + ": ");
+            oceny[i] = scanner.nextDouble();
+        }
+
+        double średnia = 0;
+        for (double ocena : oceny) {
+            średnia += ocena;
+        }
+
+        średnia /= liczbaPrzedmiotów;
+
+        System.out.println("Średnia ocen: " + średnia);
     }
 }
+
